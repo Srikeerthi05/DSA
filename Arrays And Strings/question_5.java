66ee30bf5a89fb6ba1621158a2e9dd20c109cf95
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-04-23 06:41:31 +0530

    Update question_5.java

--- Arrays And Strings/question_5.java
+++ Arrays And Strings/question_5.java
@@ -1,17 +1,34 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int m=nums.length;
-        int a[]=new int[m];
-        for (int i=0;i<m;i++){
-            int count =0;
-            for(int j=0;j<m;j++){
-                if(nums[j]<nums[i]){
-                    count++;
-                }
-            }
-            a[i]=count;
-        }
-          return a;
+        // int m=nums.length;
+        // int a[]=new int[m];
+        // for (int i=0;i<m;i++){
+        //     int count =0;
+        //     for(int j=0;j<m;j++){
+        //         if(nums[j]<nums[i]){
+        //             count++;
+        //         }
+        //     }
+        //     a[i]=count;
+        // }
+        //   return a;
+
+        // using count sort
+        int count[]=new int[101];
+        for (int n:nums]{
+            count[n]++;
     }
-   
-}
+    for(int i=1;i<101;i++){
+        count[i]+=count[i-1];
+    }
+    int res=new int[n.length];
+        for(int i=0;i<nums.length;i++){
+            int n=nums[i];
+            if(num==0){
+                res[i]=0;
+            }
+            else{
+                res[i]=count[n-1];
+            }
+        return res;
+        }
