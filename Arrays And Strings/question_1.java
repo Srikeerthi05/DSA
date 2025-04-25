45216aa50b2d5044f8b74f4f4aafc24e3b9bf874
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-04-25 11:21:11 +0530

    Update question_1.java

--- Arrays And Strings/question_1.java
+++ Arrays And Strings/question_1.java
@@ -11,3 +11,15 @@ class Solution {
         return false;
     }
 }
+// brute force approach
+// class Solution{
+//     public boolean containsDuplicate(int[] nums){
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]==nums[j]){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
