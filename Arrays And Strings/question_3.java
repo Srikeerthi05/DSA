6ce6d11733b9bee958c5beeb72e82e96891e2a66
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-04-25 11:31:57 +0530

    Update question_3.java

--- Arrays And Strings/question_3.java
+++ Arrays And Strings/question_3.java
@@ -1,19 +1,39 @@
-class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+//optimal
+class Solution{
+    public List<Integer> findDisappearedNumbers(int[] nums){
         List<Integer> res=new ArrayList<>();
-        for (int i=0;i<nums.length;i++){
-            while(nums[i]!=nums[nums[i]-1]){
-                int ci=nums[i]-1;
-                int temp=nums[i];
-                nums[i]=nums[ci];
-                nums[ci]=temp;
+        for(int i=0;i<nums.length;i++){
+            int index=Math.abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index]=-nums[index];
             }
         }
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=i+1){
+            if(nums[i]>0){
                 res.add(i+1);
             }
         }
         return res;
     }
 }
+
+//brute force
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         List<Integer> res=new ArrayList<>();
+//         for (int i=0;i<nums.length;i++){
+//             while(nums[i]!=nums[nums[i]-1]){
+//                 int ci=nums[i]-1;
+//                 int temp=nums[i];
+//                 nums[i]=nums[ci];
+//                 nums[ci]=temp;
+//             }
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=i+1){
+//                 res.add(i+1);
+//             }
+//         }
+//         return res;
+//     }
+// }
