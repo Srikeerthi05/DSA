18ba3a1fa3afcbbbf6a2f2b160b9f164d889060f
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-04-25 11:25:14 +0530

    Update question_2.java

--- Arrays And Strings/question_2.java
+++ Arrays And Strings/question_2.java
@@ -1,3 +1,4 @@
+// optimal solution
 class Solution {
     public int missingNumber(int[] nums) {
         int sum=0;
@@ -11,3 +12,23 @@ class Solution {
         
     }
 }
+
+// brute force
+// class Solution{
+//     public int missingNumber(int[] nums){
+//         int n=nums.length;
+//         for(int i=0;i<=n;i++){
+//             boolean found=false;
+//             for(int j=0;j<n;j++){
+//                 if(nums[j]==i){
+//                     found=true;
+//                     break;
+//                 }
+//             }
+//             if(!found){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
