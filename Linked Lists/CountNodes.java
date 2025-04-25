2c3f15a340b9d72d891f7ac4ec58dd62aeb314fc
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-04-25 12:09:25 +0530

    Create CountNodes.java

---
+++ Linked Lists/CountNodes.java
@@ -0,0 +1,13 @@
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        // code here
+        int count=0;
+        Node temp=head;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+}
