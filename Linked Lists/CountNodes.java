dc785e1e3a212f7712b776dd7e7af7d7250d7afa
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-04-27 23:32:07 +0530

    Update CountNodes.java

--- Linked Lists/CountNodes.java
+++ Linked Lists/CountNodes.java
@@ -1,7 +1,7 @@
 class Solution {
-    // Function to count nodes of a linked list.
+    
     public int getCount(Node head) {
-        // code here
+        
         int count=0;
         Node temp=head;
         while(temp!=null){
