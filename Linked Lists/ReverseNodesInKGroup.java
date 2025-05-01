43870f46de87c213dc9ccc785b6669c60e22f861
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-05-01 20:17:04 +0530

    Create ReverseNodesInKGroup.java

---
+++ Linked Lists/ReverseNodesInKGroup.java
@@ -0,0 +1,27 @@
+// Recursive method
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+      ListNode temp=head;
+      int count=0;
+      while(count<k){
+        if(temp==null){
+            return head;
+        }
+        temp=temp.next;
+        count++;
+      }  
+    
+      ListNode prev=reverseKGroup(temp,k);
+
+      temp=head;
+      count=0;
+      while(count<k){
+        ListNode next=temp.next;
+        temp.next=prev;
+        prev=temp;
+        temp=next;
+        count++;
+      }
+      return prev;
+    } 
+};
