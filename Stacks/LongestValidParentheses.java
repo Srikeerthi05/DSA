a8318fba30cec579b8e81ee13c66aa94ce45692a
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-05-02 16:03:50 +0530

    Create LongestValidParentheses.java

---
+++ Stacks/LongestValidParentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+      Stack<Integer> stk=new Stack<>();
+      int ml=0;
+      stk.push(-1);
+      for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='('){
+            stk.push(i);
+        }
+        else{
+            stk.pop();
+            if(stk.isEmpty()){
+                stk.push(i);
+                
+            }
+            else{
+                ml=Math.max(ml,(i-stk.peek()));
+            }
+        }
+      }  
+      return ml;
+    }
+}
