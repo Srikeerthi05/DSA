9d1ee89a62b7f94b1d2abee06bb8581818f4d934
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-07-19 06:20:58 +0530

    Update maximum-product-subarray.java

--- Arrays And Strings/maximum-product-subarray.java
+++ Arrays And Strings/maximum-product-subarray.java
@@ -1,26 +1,27 @@
-class Solution {
-    public int maxProduct(int[] nums) {
+class Main {
+    public static int maxProduct(int[] nums) {
         int mp=Integer.MIN_VALUE;
         int cp=1;
         for(int i=0;i<nums.length;i++){
             cp=cp*nums[i];
-             mp=Math.max(cs,ms);
+             mp=Math.max(cp,mp);
             if(cp==0){
                 cp=1;
-            }
-             
-            
+            }     
         }
         cp=1;
         for(int i=nums.length-1;i>=0;i--){
             cp=cp*nums[i];
-             ms=Math.max(cp,mp);
+             mp=Math.max(cp,mp);
             if(cp==0){
                 cp=1;
             }
-             
-            
         }
         return mp;
     }
+    
+    public static void main(String args[]){
+        int nums[]={2,3,5,1};
+        System.out.println(maxProduct(nums));
+        }   
 }
