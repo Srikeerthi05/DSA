314e4676b42b50bbe5c18f22e772690a7772a97f
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-07-23 23:45:17 +0530

    Update product-of-array-except-self.java

--- Arrays And Strings/product-of-array-except-self.java
+++ Arrays And Strings/product-of-array-except-self.java
@@ -15,3 +15,33 @@ class Solution {
     }
     return ans;
     }
+
+    // optimized
+    import java.util.*;
+class Solution {
+    public static int[] productExceptSelf(int[] nums) {
+        int prod=1;
+        int n=nums.length;
+        int ans[]=new int[n];
+        int prefix[]=new int[n];
+        int suffix[]=new int[n];
+        prefix[0] = 1;
+        suffix[n - 1] = 1;
+       for(int i=1;i<nums.length;i++){
+           prefix[i]=prefix[i-1]*nums[i-1];
+       }
+       for(int i=n-2;i>=0;i--){
+           suffix[i]=suffix[i+1]*nums[i+1];
+       }
+        for(int i=0;i<nums.length;i++){
+           ans[i]=prefix[i]*suffix[i];
+       }
+
+    return ans;
+    }
+    public static void main(String args[]){
+        int nums[] = {1, 2, 3, 4};
+        int[] result = productExceptSelf(nums);
+        System.out.println(Arrays.toString(result)); 
+    }
+}
