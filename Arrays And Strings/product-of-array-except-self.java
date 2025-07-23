6e92837ab1ab5664023346785111ae9907c8ce0c
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-07-23 22:58:58 +0530

    Update product-of-array-except-self.java

--- Arrays And Strings/product-of-array-except-self.java
+++ Arrays And Strings/product-of-array-except-self.java
@@ -1,10 +1,11 @@
 // brute force
 class Solution {
     public static int[] productExceptSelf(int[] nums) {
-        int prod=1;
+         
         int n=nums.length;
         int ans[]=new int[n];
         for(int i=0;i<nums.length;i++){
+            int prod=1;
             for(int j=0;j<nums.length;j++){
                 if(i!=j){
                     prod*=nums[j];
