f37bbef40d8fb6810c5348a6380b174a36a4eb83
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-01 22:54:03 +0530

    Update Max-Path-Sum.java

--- Binary Trees/Max-Path-Sum.java
+++ Binary Trees/Max-Path-Sum.java
@@ -44,3 +44,45 @@ class Solution {
         return Math.max(onlyNode,Math.max(nodeandleft,nodeandright));
     }
 }
+
+
+// optimal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxSum=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        findMaxGain(root);
+        return maxSum;
+    }
+
+    public int findMaxGain(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int lg=Math.max(0,findMaxGain(node.left));
+        int rg=Math.max(0,findMaxGain(node.right));
+
+
+        int curr=node.val+lg+rg;
+
+        maxSum=Math.max(maxSum, curr);
+
+        return node.val+Math.max(lg,rg);
+
+    }      
+    }
+
