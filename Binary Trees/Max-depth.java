f3176ae2eda600669acc7a21124119b771d38eb8
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-01 22:02:35 +0530

    Update Max-depth.java

--- Binary Trees/Max-depth.java
+++ Binary Trees/Max-depth.java
@@ -25,3 +25,59 @@ class Solution {
         
     }
 }
+
+
+
+
+
+
+// optimal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+         if(root==null){
+            return 0;
+         }
+        //  int l=maxDepth(root.left);
+        //  int r=maxDepth(root.right);
+
+        //  return 1+Math.max(l,r);
+
+    Queue<TreeNode> q=new LinkedList<>();
+    q.offer(root);
+    int depth=0;
+
+
+    while(!q.isEmpty()){
+        int lsize=q.size();
+        for(int i=0;i<lsize;i++){
+            TreeNode curr=q.poll();
+
+            if(curr.left!=null){
+                q.offer(curr.left);
+            }
+            if(curr.right!=null){
+                q.offer(curr.right);
+            }
+        }
+        depth++;
+    }
+
+    return depth;
+        
+    }
+}
