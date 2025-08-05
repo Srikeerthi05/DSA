47a58caa38c2e79dabc0e27d1b314909397c7357
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-05 22:33:47 +0530

    Update Queue-using-stacks.java

--- Queues/Queue-using-stacks.java
+++ Queues/Queue-using-stacks.java
@@ -7,6 +7,7 @@
         public static boolean isEmpty(){
         return s1.isEmpty();
         }
+      
         public static void add(int data){
             while(!s1.isEmpty()){
                 s2.push(s1.pop());
@@ -16,12 +17,14 @@
             s1.push(s2.pop());
             }
         }
+      
         public static int remove(){
             if(isEmpty()){
                 return -1;
             }
             return s1.pop();
         }
+      
         public static int peek(){
             if(isEmpty()){
                 return -1;
@@ -29,6 +32,7 @@
             return s1.peek();
         }
     }
+    
     public static void main(String args[]){
         Queue q=new Queue();
        
