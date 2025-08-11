8003871c2ea7ce836f59578f00f1eca7ad73cad3
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-11 22:15:20 +0530

    Create CountNoOfZeros(Optimal).java
    we use binary search here

---
+++ Arrays And Strings/CountNoOfZeros(Optimal).java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    int countZeroes(int[] arr) {
+        // code here
+        int l=0;
+        int h=arr.length-1;
+        int count=0;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr[mid]==0){
+                count+=(mid-l)+1;
+                l=mid+1;
+                
+            }
+            else{
+                h=mid-1;
+            }
+        }
+        return count;
+    }
+}
