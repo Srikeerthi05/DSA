7cbd834a1c9d4f8baf5d62607a79f5ed6f7086a2
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-14 23:39:00 +0530

    Update ArraysIsSorted.java

--- Recursion/ArraysIsSorted.java
+++ Recursion/ArraysIsSorted.java
@@ -15,9 +15,8 @@
         }
         else{
             return false;
-        }//
+        } //
     }
-    
     public static void main(String[] args) {
         int arr[]={3,4,1,23};
         System.out.println(sorted(arr,0));
