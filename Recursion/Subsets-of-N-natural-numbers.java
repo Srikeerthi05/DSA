48bc8a3524ee3bb4cc3596930b2ad25e4e61bf6b
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-17 00:00:53 +0530

    Create Subsets-of-N-natural-numbers.java

---
+++ Recursion/Subsets-of-N-natural-numbers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main {
+    public static void print(ArrayList<Integer> set){
+        for(int i=0;i<set.size();i++){
+            System.out.print(set.get(i)+" ");
+        }
+    }
+    
+    public static void subsets(int n, ArrayList<Integer> set){
+        if(n==0){
+            print(set);
+            return;
+        }
+        
+        //to add--to be
+        set.add(n);
+        subsets(n-1,set);
+        //to remove--to not be
+        set.remove(set.size()-1);
+        subsets(n-1,set);
+         
+    }
+    public static void main(String[] args) {
+        int n=3;
+        ArrayList<Integer> set=new ArrayList<>();
+         subsets(n,set);
+    }
+}
