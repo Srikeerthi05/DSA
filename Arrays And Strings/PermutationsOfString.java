cea25b97f242410c63b9e252d5cdda5ec4fd9597
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-18 12:59:38 +0530

    Create PermutationsOfString.java

---
+++ Arrays And Strings/PermutationsOfString.java
@@ -0,0 +1,23 @@
+//optimal
+class Solution {
+    public static void permute(char[] str, int l, int r) {
+        if(l==r){
+            System.out.println(new String(str));
+        }
+       for(int i=l;i<r;i++){
+           swap(str,l,i);
+           permute(str,l+1,r);
+           swap(str,l,i);
+       }
+    }
+       public static void swap(char str[],int i,int j){
+           char temp=str[i];
+           str[i]=str[j];
+           str[j]=temp;
+       }
+
+    public static void main(String[] args) {
+        String s = "ABC";
+        permute(s.toCharArray(), 0, s.length() - 1);
+    }
+}
