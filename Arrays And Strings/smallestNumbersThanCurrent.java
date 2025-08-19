8fdc4613c3e66a2e0a011ce57c391de0bf5ad885
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-19 08:31:07 +0530

    Update and rename question_5.java to smallestNumbersThanCurrent.java

--- Arrays And Strings/smallestNumbersThanCurrent.java
+++ Arrays And Strings/smallestNumbersThanCurrent.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        // int m=nums.length;
-        // int a[]=new int[m];
-        // for (int i=0;i<m;i++){
-        //     int count =0;
-        //     for(int j=0;j<m;j++){
-        //         if(nums[j]<nums[i]){
-        //             count++;
-        //         }
-        //     }
-        //     a[i]=count;
-        // }
-        //   return a;
+        int m=nums.length;
+        int a[]=new int[m];
+        for (int i=0;i<m;i++){
+            int count =0;
+            for(int j=0;j<m;j++){
+                if(nums[j]<nums[i]){
+                    count++;
+                }
+            }
+            a[i]=count;
+        }
+          return a;
 
-        // using count sort
+        //using count sort
         int count[]=new int[101];
         for (int n:nums]{
             count[n]++;
