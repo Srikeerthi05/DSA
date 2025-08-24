493e48c7a1fc5cd5dc04faffbb8df504b99966df
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-24 13:16:45 +0530

    Create InsertionSort.java

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main{
+    static void InsertionSort(int arr[]){
+        int n=arr.length;
+         for(int i=1;i<n;i++){
+             int curr=arr[i];
+             int prev=i-1;
+             while(prev>=0 && arr[prev]>curr){
+                 arr[prev+1]=arr[prev];
+                 prev--;
+             }
+             arr[prev+1]=curr;
+         }
+    }
+    public static void main(String args[]){
+        int arr[]={14,9,15,12,6,8,13};
+        InsertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
