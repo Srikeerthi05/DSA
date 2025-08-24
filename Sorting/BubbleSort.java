5e6157595cf2974cc4b9ac1223b7636f893b10af
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-24 11:29:13 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -2,8 +2,8 @@ import java.util.*;
 class Main{
     static void BubbleSort(int arr[]){
         int n=arr.length;
-        for(int i=n-1;i>=1;i--){
-            for(int j=0;j<=i-1;j++){
+        for(int i=n-1;i>=1;i--){  //for(int i=0;i<n-1;i++)
+            for(int j=0;j<=i-1;j++){ //for(int j=0;j<n-1-i;j++)
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
