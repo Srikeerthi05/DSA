3fad556f4067c0031a2c770b1a7b5c9481c37318
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-28 23:51:51 +0530

    Create NoOfTimesArrayIsRotated.java

---
+++ BinarySearch/NoOfTimesArrayIsRotated.java
@@ -0,0 +1,64 @@
+//brute-force
+import java.util.*;
+public class tUf {
+    public static int findKRotation(int[] arr) {
+         int n=arr.length;
+         int index=1;
+         int ans=Integer.MAX_VALUE;
+         for(int i=0;i<n;i++){
+             if(arr[i]<ans){
+                 ans=arr[i];
+                 index=i;
+                 }
+           low=mid+1;
+         }
+         return index;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findKRotation(arr);
+        System.out.println("The array is rotated " + ans + " times.");
+    }
+}
+
+//optimal
+import java.util.*;
+public class tUf {
+    public static int findKRotation(int[] arr) {
+         int n=arr.length;
+         int ans=Integer.MAX_VALUE; //to store smallest value
+         int index=-1; //to store index of smallest value
+         int low=0;
+         int high=n-1;
+         while(low<=high){
+             int mid=(low+high)/2;
+             if(arr[low]<=arr[high]){
+                 if(arr[low]<ans){
+                     index=low;
+                     ans=arr[low];
+                 }
+                 break;
+             }
+             if(arr[low]<=arr[mid]){
+                 if(arr[low]<ans){
+                     index=low;
+                     ans=arr[low];
+                 }
+                 low=mid+1;
+             }
+             else{
+                 if(arr[mid]<ans){
+                     index=mid;
+                     ans=arr[mid];
+                 }
+                 high=mid-1;
+             }
+         }
+        return index;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int ans = findKRotation(arr);
+        System.out.println("The array is rotated " + ans + " times.");
+    }
+}
