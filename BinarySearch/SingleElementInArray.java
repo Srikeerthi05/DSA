c8bedcc590726e44470f29ec19507e4da8923054
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-08-29 21:12:08 +0530

    Update SingleElementInArray.java

--- BinarySearch/SingleElementInArray.java
+++ BinarySearch/SingleElementInArray.java
@@ -1,4 +1,4 @@
-//brute force
+//brute force-1
 class Main {
     public static int singleElement(int arr[]){
         int n=arr.length;
@@ -21,3 +21,50 @@ class Main {
         }
         return -1;
     }
+}
+
+
+//brute force 2
+class Main {
+    public static int singleElement(int arr[]){
+        int n=arr.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=ans^arr[i];
+        }
+        return ans;
+    }
+}
+
+//optimal
+class Main {
+    public static int singleElement(int arr[]){
+        int n=arr.length;
+        if(n==1){
+            return arr[0];
+        }
+        if(arr[0]!=arr[1]){
+            return arr[0];
+        }
+        if(arr[n-1]!=arr[n-2]){
+            return arr[n-1];
+        }
+        int low=1;
+        int high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]!=arr[mid]+1 && arr[mid]!=arr[mid]-1){   //check left and right of mid
+                return arr[mid];
+            }
+            if(mid%2==0 && arr[mid]==arr[mid]+1 || mid%2==1 && arr[mid]!=arr[mid]-1){  //if mid is even,check next element OR if mid is odd,check previous element
+                low=mid+1;
+            }
+            high=mid-1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,1,3,5,5};
+        System.out.println(singleElement(arr));
+    }
+}
