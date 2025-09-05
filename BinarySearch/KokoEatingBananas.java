793472f2f2e0c3826a1ff9eba8f39efd6bc42989
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-05 22:14:43 +0530

    Update KokoEatingBananas.java

--- BinarySearch/KokoEatingBananas.java
+++ BinarySearch/KokoEatingBananas.java
@@ -1,3 +1,4 @@
+//brute-force
 import java.util.*;
 public class tUf {
     public static int findMax(int[] v) {
@@ -32,5 +33,41 @@ public class tUf {
         int h = 8;
         int ans = minimumRateToEatBananas(v, h);
         System.out.println("Koko should eat at least " + ans + " bananas/hr.");
+    }
+}
+
+//optimal
+class Solution {
+    public int maxi(int piles[]){
+        int max=Integer.MIN_VALUE;
+        int n=piles.length;
+        for(int i=0;i<n;i++){
+            max=Math.max(piles[i],max);
+        }
+        return max;
+    }
+    public int calcHours(int piles[],int speed){
+        int total=0;
+        int n=piles.length;
+        for(int i=0;i<n;i++){
+            total+=Math.ceil((double)(piles[i])/(double)speed);
+        }
+        return total;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int n=piles.length;
+        int low=1;
+        int high=maxi(piles);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int totalHrs=calcHours(piles,mid);
+            if(totalHrs<=h){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;  
     }
 }
