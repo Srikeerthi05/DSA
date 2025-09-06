700e2c8289d30324a1e7511add5fd9626325f3b7
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-06 20:33:07 +0530

    Update FindTheSmallestDivisorGivenAThreshold.java

--- BinarySearch/FindTheSmallestDivisorGivenAThreshold.java
+++ BinarySearch/FindTheSmallestDivisorGivenAThreshold.java
@@ -23,3 +23,42 @@ class Solution {
         return -1; 
     }
 }
+
+//optimal
+class Solution {
+    public int maxi(int[] nums){
+        int n=nums.length;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            max=Math.max(max,nums[i]);
+        }
+        return max;
+    }
+    public int div(int nums[],int d){
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=Math.ceil((double)nums[i]/(double)d);
+        }
+        return sum;
+    }
+    public int smallestDivisor(int[] nums, int threshold) {
+        int n=nums.length;
+        if(n>threshold){
+            return -1;
+        }
+        int max=maxi(nums);
+        int low=1;
+        int high=max;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(div(nums,mid)<=threshold){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        } 
+        return low; 
+    }
+}
