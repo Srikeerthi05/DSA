29216e8b229672ff1fee66d2af1934f112681552
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-06 18:36:26 +0530

    Create MinNumOfDaysToMakeMBouquets.java

---
+++ BinarySearch/MinNumOfDaysToMakeMBouquets.java
@@ -0,0 +1,39 @@
+//brute force
+class Solution {
+    public boolean possible(int[] bloomDay,int day ,int m, int k){
+        int n=bloomDay.length;
+        int count=0;
+        int noofB=0;
+        for(int i=0;i<n;i++){
+            if(bloomDay[i]<=day){
+                count++;
+            }
+            else{
+                noofB+=(count/k);
+                count=0;
+            }
+        }
+        noofB+=(count/k);
+        if(noofB>=m){
+            return true;
+        }
+        return false;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n=bloomDay.length;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            max=Math.max(bloomDay[i],max);
+            min=Math.min(bloomDay[i],min);
+        }
+        for(int i=min;i<=max;i++){
+            if(possible(bloomDay,i,m,k)){
+                return i;
+            }
+        }
+
+        return -1;
+        
+    }
+}
