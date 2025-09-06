4d978f73bec8f84f8202e0416536b1e85653e066
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-07 00:05:58 +0530

    Update KthMissingPositiveNumber.java

--- BinarySearch/KthMissingPositiveNumber.java
+++ BinarySearch/KthMissingPositiveNumber.java
@@ -3,7 +3,7 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
         int n=arr.length;
         for(int i=0;i<n;i++){
-            if(arr[i]<k){
+            if(arr[i]<=k){
                 k++;
             }
             else{
