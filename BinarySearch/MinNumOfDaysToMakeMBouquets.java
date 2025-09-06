80019fcbbb7e18b06843a9c86b4a108a230f12f0
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-06 18:51:23 +0530

    Update MinNumOfDaysToMakeMBouquets.java

--- BinarySearch/MinNumOfDaysToMakeMBouquets.java
+++ BinarySearch/MinNumOfDaysToMakeMBouquets.java
@@ -32,8 +32,53 @@ class Solution {
                 return i;
             }
         }
+        return -1;   
+    }
+}
 
-        return -1;
-        
+//optimal
+class Solution {
+    public boolean possible(int[] bloomDay,int day ,int m, int k){
+        int n=bloomDay.length;
+        int count=0;
+        int noofB=0;
+        for(int i=0;i<n;i++){
+            if(bloomDay[i]<=day){
+                count++;
+            }
+            else{
+                noofB+=(count/k);
+                count=0;
+            }
+        }
+        noofB+=(count/k);
+        if(noofB>=m){
+            return true;
+        }
+        return false;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n=bloomDay.length;
+        if(n<((long)m*k)){
+            return -1;
+        }
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            max=Math.max(bloomDay[i],max);
+            min=Math.min(bloomDay[i],min);
+        }
+       int low=min;
+       int high=max;
+       while(low<=high){
+        int mid=low+(high-low)/2;
+        if(possible(bloomDay,mid,m,k)){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+       }
+        return low; 
     }
 }
