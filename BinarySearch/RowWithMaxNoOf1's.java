7279e9dd7fe1323cab9c0a4dfea99a6080896903
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-10 00:01:46 +0530

    Create RowWithMaxNoOf1's.java

---
+++ BinarySearch/RowWithMaxNoOf1's.java
@@ -0,0 +1,29 @@
+//brute force
+import java.util.*;
+public class tUf {
+    public static int rowWithMax1s(int[][] matrix, int n, int m) {
+      int maxCount=-1;
+      int index=-1;
+      for(int i=0;i<n;i++){
+          int count=0;
+          for(int j=0;j<m;j++){
+              count+=matrix[i][j];
+          }
+          if(count>maxCount){
+              maxCount=count;
+              index=i;
+          }
+      }
+      return index;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 1},
+            {0, 0, 1},
+            {0, 0, 0}
+        };
+        int n = 3, m = 3;
+        System.out.println("The row with the maximum number of 1's is: " +rowWithMax1s(matrix, n, m));
+    }
+}
