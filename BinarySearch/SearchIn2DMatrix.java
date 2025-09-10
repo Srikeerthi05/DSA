fd96b3540cfef903bdb4cb98fd2bff9be20975ab
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-10 13:05:51 +0530

    Update SearchIn2DMatrix.java

--- BinarySearch/SearchIn2DMatrix.java
+++ BinarySearch/SearchIn2DMatrix.java
@@ -13,3 +13,34 @@ class Solution {
         return false;
     }
 }
+//better
+class Solution {
+    public boolean binarySearch(int[] mat,int target){
+        int l=mat.length;
+        int low=0;
+        int high=l-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mat[mid]==target){
+                return true;
+            }
+            else if(target<mid){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){ 
+                if(matrix[i][0]<=target && target<=matrix[i][n-1]){
+                    return binarySearch(matrix[i],target);
+                }
+        }
+        return false;
+    }
+}
