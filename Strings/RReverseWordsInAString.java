93c3709bb8e3e0803e944300afadae035c5d1605
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-22 21:31:18 +0530

    Update and rename ReverseWordsInAString.java to Strings/ReverseWordsInAString.java

--- Strings/RReverseWordsInAString.java
+++ Strings/RReverseWordsInAString.java
@@ -27,3 +27,14 @@ class Solution {
          return ans;
     } 
 }
+//another brute-force
+class Solution {
+    public String reverseWords(String s) {
+        String[] str=s.trim().split("\\s+");
+        String out="";
+        for(int i=str.length-1;i>0;i--){
+            out+=str[i]+" ";
+        }
+        return out+str[0];
+    }
+}
