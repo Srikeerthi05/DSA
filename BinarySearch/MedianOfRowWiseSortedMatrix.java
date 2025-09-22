a1247f2cba61f8d42210e7208c75b3690920a0f2
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-22 19:58:28 +0530

    Update MedianOfRowWiseSortedMatrix.java

--- BinarySearch/MedianOfRowWiseSortedMatrix.java
+++ BinarySearch/MedianOfRowWiseSortedMatrix.java
@@ -22,3 +22,52 @@ public class tUf {
         System.out.println("The median element is: " + ans);
     }
 }
+//optimal
+import java.util.*;
+class tUf {
+    static int upperBound(int[] arr, int x, int n) {
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] > x) {
+                ans = mid;
+                // look for a smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    static int countSmallEqual(int[][] matrix, int m, int n, int x) {
+        int count=0;
+        for(int i=0;i<m;i++){
+            count+=upperBound(matrix[i],x,n);
+        }
+       return count;
+    }
+
+    static int median(int[][] matrix, int m, int n) {
+         int low=Integer.MAX_VALUE;
+         int high=Integer.MIN_VALUE;
+         int req=(m*n)/2;
+         for (int i = 0; i < m; i++) {
+            low = Math.min(low, matrix[i][0]);
+            high = Math.max(high, matrix[i][n - 1]);
+        }
+         while(low<=high){
+             int mid=(low+high)/2;
+             int smallerEquals=countSmallEqual(matrix,m,n,mid);
+             if(smallerEquals<=req){
+                 low=mid+1;
+             }
+             else{
+                 high=mid-1;
+             }
+         }
+         return low;
+    }
+}
