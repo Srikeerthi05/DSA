df3d8d383923911a041af4c7ab7c4eae55a18431
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-24 16:00:47 +0530

    Update ReverseWordsInAString.java

--- Strings/ReverseWordsInAString.java
+++ Strings/ReverseWordsInAString.java
@@ -39,3 +39,39 @@ class Solution {
         return out+str[0];
     }
 }
+
+//optimal
+ import java.io.*;
+class Test{
+static private String result(String s){ 
+    String temp="";
+    String ans="";
+    int left=0;
+    int right=s.length()-1;
+    while(left<=right){
+        char ch=s.charAt(left);
+        if(ch!=' '){
+            temp+=ch;
+             
+        }
+        else if(ch==' '){
+            if(!ans.equals("")){
+                ans=temp+" "+ans;
+            }
+            else{
+                ans=temp;
+            }
+            temp="";
+        }
+        left++;
+    }
+    if(!temp.equals("")){
+        if(!ans.equals("")){
+            ans=temp+" "+ans;
+        }
+        else{
+            ans=temp;
+        }
+    }
+    return ans;
+}
