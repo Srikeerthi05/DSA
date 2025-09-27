91977b0394b4e5019b44b754d70cc8f62c05a03f
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-27 18:47:31 +0530

    Update SortCharacterByFrequency.java

--- Strings/SortCharacterByFrequency.java
+++ Strings/SortCharacterByFrequency.java
@@ -26,3 +26,35 @@ class Solution {
         
     }
 }
+
+//method-2(Bucket Sort)
+class Solution {
+    public String frequencySort(String s) {
+        int freq[]=new int[128];
+        for(char ch:s.toCharArray()){
+            freq[ch]++;
+        }
+        
+        List<Character> buckets[]=new ArrayList[s.length()+1];
+        for(int i=0;i<=s.length();i++){
+            buckets[i]=new ArrayList<>();
+        }
+
+        for(char c=0;c<128;c++){
+            if(freq[c]>0){
+                buckets[freq[c]].add(c);
+            }
+        }
+
+        StringBuilder sb=new StringBuilder();
+        for(int i=s.length();i>=1;i--){
+            for(char c:buckets[i]){
+                for(int j=0;j<i;j++){
+                    sb.append(c);
+                }
+            }
+        }
+       return sb.toString();
+    }
+}
+
