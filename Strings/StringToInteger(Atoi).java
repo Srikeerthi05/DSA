b8dd60a4044dfa3f410919a1cbad44a1ac295085
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-28 20:44:22 +0530

    Update StringToInteger(Atoi).java

--- Strings/StringToInteger(Atoi).java
+++ Strings/StringToInteger(Atoi).java
@@ -31,5 +31,43 @@ class Solution {
         }
         return (int)num;
     }
-     
+}
+
+//optimal
+class Solution {
+    public int myAtoi(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+        long res=0;
+        int sign=1;
+        int i=0;
+        while(i<s.length() && s.charAt(i)==' '){
+            i++;
+        }
+            if(i==s.length()){
+                return 0;
+            }
+            if(s.charAt(i)=='-'){
+                sign=-1;
+                i++;
+            }
+            else if(s.charAt(i)=='+'){
+                i++;
+            }
+        
+
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
+            int digit=s.charAt(i)-'0';
+            res=res*10+digit;
+            if(sign*res<Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }
+            if(sign*res>Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
+            }
+            i++;
+        }
+        return (int)(sign*res);
+    }
 }
