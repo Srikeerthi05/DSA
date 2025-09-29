024e0236c90f5391cb87c78f7e716bf3fa25c8c8
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-29 22:18:29 +0530

    Create Create-Print-LL.java

---
+++ Linked Lists/Create-Print-LL.java
@@ -0,0 +1,27 @@
+class LinkedList{
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    
+    public void printList(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+"-> ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String args[]){
+        LinkedList ll=new LinkedList();
+        ll.head=new Node(1);
+        ll.head.next=new Node(2);
+        ll.printList();
+    }
+}
