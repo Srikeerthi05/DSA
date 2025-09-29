27ad3074b942f3c1f0a37bb19819f948bc934ece
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-29 16:32:25 +0530

    Update SumOfBeautyOfAllSubstrings.java

--- Strings/SumOfBeautyOfAllSubstrings.java
+++ Strings/SumOfBeautyOfAllSubstrings.java
@@ -30,3 +30,29 @@ class Solution {
     return sum;
 }
 }
+
+//optimal
+class Solution {
+    public int beautySum(String s) {
+        int n=s.length();
+        int sum=0;
+        for(int i=0;i<n;i++){
+             int freq[]=new int[26];
+             for(int j=i;j<n;j++){
+                char c=s.charAt(j);
+                freq[c-'a']++;
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        for(int f:freq){
+            if(f>0){
+                max=Math.max(max,f);
+                min=Math.min(min,f);
+            }
+
+        }
+        sum+=max-min;
+     }
+    }
+    return sum;
+ }
+}
