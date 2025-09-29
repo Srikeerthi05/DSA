9627d29dec61f0410b2ef51e1b1120cc6f0cada9
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-29 20:27:14 +0530

    Update MaxNestingDepthOfParanthesis.java

--- Strings/MaxNestingDepthOfParanthesis.java
+++ Strings/MaxNestingDepthOfParanthesis.java
@@ -15,3 +15,21 @@ class Solution {
         return maxD;
     }
 }
+
+//optimal
+class Solution {
+    public int maxDepth(String s) {
+        int curr=0;
+        int max=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='('){
+                curr++;
+                max=Math.max(curr,max);
+            }
+            else if(ch==')'){
+                curr--;
+            }
+        }
+        return max;
+    }
+}
