987ed4af1e2b59f7671cd64ea37fa07306b84535
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-09-30 22:21:58 +0530

    Create TraverseLL.java

---
+++ Linked Lists/TraverseLL.java
@@ -0,0 +1,24 @@
+class LinkedList{
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail; 
+  
+    public static void main(String args[]){
+        LinkedList ll=new LinkedList();
+        ll.head=new Node(1);
+        ll.head.next=new Node(2);
+        ll.head.next.next=new Node(3); 
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+}
