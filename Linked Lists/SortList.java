f698402469ae43e4e9d7e96ba71c03a1c0256fd2
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-11-07 15:38:16 +0530

    Brute(list) & optimal(Merge sort)

--- Linked Lists/SortList.java
+++ Linked Lists/SortList.java
@@ -1,4 +1,33 @@
-  
+//brute
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+    ListNode temp=head;
+    List<Integer> list=new ArrayList<>();
+    while(temp!=null){
+        list.add(temp.val);
+        temp=temp.next;
+    }
+    Collections.sort(list);
+    temp=head;
+    for(int val:list){
+        temp.val=val;
+        temp=temp.next;
+    }
+    return head;
+    }
+}
+
+//Merge sort
 class Solution { 
     public ListNode sortList(ListNode head) {
         if(head==null||head.next==null){
