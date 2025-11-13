ea89cec4dafabca9bacbc0681de47998ce8a334b
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-11-13 06:32:11 +0530

    Implement intersection detection for two linked lists

---
+++ Linked Lists/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,30 @@
+//brute
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp=headA;
+        HashMap<ListNode, Integer> mp=new HashMap<>();
+        while(temp!=null){
+            mp.put(temp, 1);
+            temp=temp.next;
+        }
+        temp=headB;
+        while(temp!=null){
+            if(mp.containsKey(temp)){
+                return temp;
+            }
+            temp=temp.next;
+        }
+        return null;       
+    }
+}
