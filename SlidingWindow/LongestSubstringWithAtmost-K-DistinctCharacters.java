35cc3056c4c4fea36ca3444b0e7dd81784a6fb1e
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-01-05 20:48:36 +0530

    brute and optimal(sliding window+2 pointer)

--- SlidingWindow/LongestSubstringWithAtmost-K-DistinctCharacters.java
+++ SlidingWindow/LongestSubstringWithAtmost-K-DistinctCharacters.java
@@ -26,3 +26,40 @@ public class Solution {
         System.out.println(lengthOfLongestSubstringKDistinct(s2, k2)); 
     }
 }
+
+
+//optimal
+import java.util.*;
+public class Solution {
+    public static int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int n=s.length();
+        int maxLen=0;
+        int l=0;
+        int r=0;
+        HashMap<Character, Integer> map=new HashMap<>();
+        while(r<n){
+            char ch=s.charAt(r);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            while(map.size()>k){
+                char left=s.charAt(l);
+                map.put(left,map.get(left)-1);
+                if(map.get(left)==0){
+                    map.remove(left);
+                }
+                l++;
+            }
+                maxLen=Math.max(maxLen, r-l+1);
+                r=r+1;
+        }
+        return maxLen;
+        
+    }
+    public static void main(String[] args) {
+        String s1 = "eceba";
+        int k1 = 2;
+        System.out.println(lengthOfLongestSubstringKDistinct(s1, k1));
+        String s2 = "aa";
+        int k2 = 1;
+        System.out.println(lengthOfLongestSubstringKDistinct(s2, k2)); 
+    }
+}
