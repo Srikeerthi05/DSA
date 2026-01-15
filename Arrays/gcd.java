dec20fc6c608c305ec1308bc009918f72aec2cce
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-01-15 19:25:32 +0530

    Refactor GCD calculation in gcd.java

--- Arrays/gcd.java
+++ Arrays/gcd.java
@@ -13,3 +13,19 @@ class Main{
    System.out.println(gcd);
  }
 }
+//better
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+    int n1=9;
+    int n2=12;
+    int gcd=1;
+    for(int i=Math.min(n1,n2);i>=1;i--){
+        if(n1%i==0 && n2%i==0){
+            gcd=i;
+            break;
+    }
+  }
+   System.out.println(gcd);
+ }
+}
