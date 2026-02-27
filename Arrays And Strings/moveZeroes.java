7cbee594f9699c75953e4c038226ea0d50af8dfa
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-02-27 23:29:34 +0530

    Add brute-force and start optimal zeroes mover
    Implemented a brute-force method to move zeroes in an array and started an optimal solution.

--- Arrays And Strings/moveZeroes.java
+++ Arrays And Strings/moveZeroes.java
@@ -1,3 +1,28 @@
+//brute
+import java.util.*;
+class Main {
+public static void main(String[] args) {
+Scanner sc=new Scanner([System.in](http://system.in/));
+int arr[]={4,1,0,5,6,7};
+int temp[]=new int[arr.length];
+int index=0;
+for(int i=0;i<arr.length;i++){
+if(arr[i]!=0){
+temp[index]=arr[i];
+index++;
+}
+}
+for(int j=0;j<temp.length;j++){
+arr[j]=temp[j];
+}
+int nonzero=index;
+for(int k=nonzero;k<arr.length;k++){
+arr[k]=0;
+}
+System.out.println(Arrays.toString(arr));
+}
+}
+//optimal
 class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
