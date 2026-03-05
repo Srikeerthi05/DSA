8f7de058abe82f36c527b9c05054cf58eac6300d
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-03-05 21:03:37 +0530

    Update OddOccurences.java

--- Arrays And Strings/OddOccurences.java
+++ Arrays And Strings/OddOccurences.java
@@ -18,3 +18,26 @@ class Main {
         }
     }
 }
+//optimal
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,2,3,1,3};
+        int n=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            if(entry.getValue()%2!=0){
+                System.out.println(entry.getKey());
+                break;
+            }
+        }
+    }
+}
