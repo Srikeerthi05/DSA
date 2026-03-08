1c2bfbf3e6636d4b5369df3a3ec969d8811927b0
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-03-08 18:05:32 +0530

    Implement main method for symmetric pairs detection
    Added a main method to read an array of pairs and print symmetric pairs.

--- Arrays And Strings/SymmetricPairsInArray.java
+++ Arrays And Strings/SymmetricPairsInArray.java
@@ -20,3 +20,29 @@ class Main {
         }
     }
 }
+//optimal
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n=sc.nextInt();
+        int arr[][]=new int[n][2];
+        System.out.println("Enter elements:");
+        for(int i=0;i<n;i++){
+            arr[i][0]=sc.nextInt();
+            arr[i][1]=sc.nextInt();
+        }
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int first=arr[i][0];
+            int second=arr[i][1];
+            if(map.containsKey(second) && map.get(second)==first){
+                System.out.println(first+ "," +second);
+            }
+            else{
+                map.put(first,second);
+            }
+        }
+    }
+}
