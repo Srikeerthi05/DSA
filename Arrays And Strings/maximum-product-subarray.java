b6030ce1c7dbb181f5378585e2c0002907c7d4fd
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-03-09 23:57:24 +0530

    Add user input for maximum product subarray calculation

--- Arrays And Strings/maximum-product-subarray.java
+++ Arrays And Strings/maximum-product-subarray.java
@@ -23,3 +23,26 @@ class Main {
         System.out.println("Maxim product of subarray : "+max);
     }
 }
+//better
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+             int prod=1;
+            for(int j=i;j<n;j++){
+                prod=prod*arr[j];
+                max=Math.max(max,prod);
+            }
+        }
+        System.out.println("Maxim product of subarray : "+max);
+    }
+}
