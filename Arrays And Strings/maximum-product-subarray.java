88737cd5d54fe0ed1d850a8c0229859b6e3745ab
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-03-10 00:11:04 +0530

    Implement maximum product subarray calculation
    Added a new implementation to calculate the maximum product of a subarray using prefix and suffix products.

--- Arrays And Strings/maximum-product-subarray.java
+++ Arrays And Strings/maximum-product-subarray.java
@@ -46,3 +46,33 @@ class Main {
         System.out.println("Maxim product of subarray : "+max);
     }
 }
+//optimal
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max=Integer.MIN_VALUE;
+        int prefix=1;
+        int suffix=1;
+        for(int i=0;i<n;i++){
+            if(prefix==0){
+                prefix=1;
+            }
+            if(suffix==0){
+                suffix=1;
+            }
+            prefix=prefix*arr[i];
+            suffix=suffix*arr[n-i-1];
+            
+            max=Math.max(max, Math.max(prefix, suffix));
+        }
+        System.out.println("Maxim product of subarray : "+max);
+    }
+}
