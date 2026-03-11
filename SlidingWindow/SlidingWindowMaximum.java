616cdba096af87729716b9e5e22ffd742372abc2
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-03-11 20:58:03 +0530

    sliding window maximum using deque and brute
    Added an optimal solution for finding the maximum in a sliding window using a deque.

--- SlidingWindow/SlidingWindowMaximum.java
+++ SlidingWindow/SlidingWindowMaximum.java
@@ -23,3 +23,37 @@ class Main {
        System.out.print("Maximum in the window is: "+list);
     }
 }
+
+//optimal-using deque
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the size of the array: ");
+       int n=sc.nextInt();
+       System.out.println("Enter the window size: ");
+       int k=sc.nextInt();
+       int arr[]=new int[n];
+       System.out.println("Enter the elements of the array: ");
+       for(int i=0;i<n;i++){
+           arr[i]=sc.nextInt();
+       }
+       List<Integer> list=new ArrayList<>();
+       Deque<Integer> dq=new LinkedList<>();
+       for(int i=0;i<n;i++){
+           if(!dq.isEmpty()&& dq.peekFirst()<=i-k){
+               dq.pollFirst();
+           }
+           while(!dq.isEmpty() && arr[dq.peekLast()]<=arr[i]){
+               dq.pollLast();
+            
+           }
+            dq.offerLast(i);
+            if(i>=k-1){
+           list.add(arr[dq.peekFirst()]);
+       }
+       }
+        
+       System.out.print("Maximum in the window is: "+list);
+    }
+}
