609b4dd2c880832d97fd25833109b80c242aac50
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2026-03-12 19:06:29 +0530

    Update SubArraySumEqualsK.java

--- Arrays And Strings/SubArraySumEqualsK.java
+++ Arrays And Strings/SubArraySumEqualsK.java
@@ -26,3 +26,30 @@ class Main {
        }
     }
 }
+
+//better
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.println("Enter the size of the array: ");
+       int n=sc.nextInt();
+       System.out.println("Enter the target sum: ");
+       int l=sc.nextInt();
+       System.out.println("Enter the Array Elements: ");
+       int arr[]=new int[n];
+       for(int i=0;i<n;i++){
+           arr[i]=sc.nextInt();
+       }
+       for(int i=0;i<n;i++){
+           int sum=0;
+           for(int j=i;j<n;j++){
+                sum=sum+arr[j];
+               if(sum==l){
+                   System.out.println("The Indices are: "+(i+1)+ " "+(j+1));
+                   return;
+               }
+           }
+       }
+    }
+}
